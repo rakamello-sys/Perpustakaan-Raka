61c09634d4383edf7b209c7129b70445352e3178
Angga Dev ~ <rakamelloryzin@gmail.com>
2026-04-07 07:51:57 +0700

    menambahkan public function

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
 
 class Transaction extends Model
 {
+    protected $fillable = ['user_id', 'book_id', 'tanggal_pinjam', 'tanggal_kembali', 'status'];
+
+    public function user() {return $this->belongsTo(User::class);}
+    public function book() {return $this->belongsTo(Book::class);}
     //
 }
