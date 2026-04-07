34e4b6036e2916d09993d0b36f0ab01c6d2d7daa
Angga Dev ~ <rakamelloryzin@gmail.com>
2026-04-07 10:38:20 +0700

    membuat middleware (hak akses)

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if (!Auth::check() || Auth::user()->role !== $role){
+            abort(403, 'Akses Ditolak');
+
+        }
+        return $next($request);
+    }
+}
