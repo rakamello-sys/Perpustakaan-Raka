987a0db2fe63cc00c01435474b363da84696ff36
Angga Dev ~ <rakamelloryzin@gmail.com>
2026-04-07 07:46:14 +0700

    mengubah setup model dan relasi

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    protected $fillable = ['judul', 'penulis', 'penerbit', 'kategori', 'stok'];
+
+    public function transaction () {
+        return $this->hashMany(Transaction::class);
+    }
     //
 }
